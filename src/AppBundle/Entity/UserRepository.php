b2b270e9eeb191d814f115b2d3909d97fc5e56f7
srgrover <djgrovezone@gmail.com>
2018-03-01 17:05:01 +0100

    Añadido FOSUserBundle y creada entidad Usuario extendida

---
+++ src/AppBundle/Entity/UserRepository.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Security\User\UserLoaderInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+class UserRepository extends EntityRepository implements UserLoaderInterface
+{
+    public function loadUserByUsername($username) {
+        if (!$username) {
+            return null;
+        }
+        return $this->getEntityManager()
+            ->createQuery('SELECT u FROM AppBundle:Usuario u 
+                           WHERE u.loginUsername = :username
+                           OR u.email = :username')
+            ->setParameters([
+                'username' => $username
+            ])
+            ->getOneOrNullResult();
+    }
+    public function refreshUser(UserInterface $user) {
+        return $this->loadUserByUsername($user->getUsername());
+    }
+    public function supportsClass($class) {
+        return $class === 'AppBundle\Entity\User';
+    }
+    public function createNewUser()
+    {
+        $user = new User();
+        $this->getEntityManager()->persist($user);
+        return $user;
+    }
+    public function countAgreementHours(User $user)
+    {
+        return $this->getEntityManager()
+            ->createQuery('SELECT SUM(w.hours) FROM AppBundle:Workday w INNER JOIN w.agreement a WHERE a.student = :user')
+            ->setParameter('user', $user)
+            ->getSingleScalarResult();
+    }
+    public function countTrackedHours(User $user)
+    {
+        return $this->getEntityManager()
+            ->createQuery('SELECT SUM(t.hours) FROM AppBundle:Tracking t INNER JOIN AppBundle:Workday w INNER JOIN w.agreement a WHERE a.student = :user')
+            ->setParameter('user', $user)
+            ->getSingleScalarResult();
+    }
+    public function getAgreementRelatedStudents(User $user)
+    {
+        $em = $this->getEntityManager();
+        return $em->createQuery('SELECT DISTINCT u,
+              (SELECT SUM(t2.hours) FROM AppBundle:Tracking t2 INNER JOIN AppBundle:Workday w2 WITH t2.workday = w2 INNER JOIN w2.agreement a2 WITH w2.agreement = a2 WHERE a2.student = u),
+              (SELECT SUM(w3.hours) FROM AppBundle:Workday w3 INNER JOIN w3.agreement a3 WHERE a3.student = u)
+              FROM AppBundle:User u JOIN AppBundle:Agreement a WITH a.student = u WHERE a.educationalTutor = :user OR a.workTutor = :user ORDER BY u.lastName, u.firstName')
+            ->setParameter('user', $user)
+            ->getResult();
+    }
+    public function getEducationalTutorsAgreementSummary()
+    {
+        $em = $this->getEntityManager();
+        return $em->createQuery('SELECT u,
+              (SELECT COUNT(a1) FROM AppBundle:Agreement a1 WHERE a1.educationalTutor = u),
+              (SELECT COUNT(DISTINCT a2.student) FROM AppBundle:Agreement a2 WHERE a2.educationalTutor = u)
+              FROM AppBundle:User u WHERE u IN (SELECT DISTINCT IDENTITY(a.educationalTutor) FROM AppBundle:Agreement a) ORDER BY u.lastName, u.firstName')
+            ->getResult();
+    }
+    public function getEducationalTutorsByStudent(User $student)
+    {
+        $em = $this->getEntityManager();
+        return $em->createQuery('SELECT u FROM AppBundle:User u WHERE u IN (SELECT DISTINCT IDENTITY(a.educationalTutor) FROM AppBundle:Agreement a INNER JOIN a.educationalTutor u2 WITH a.educationalTutor = u2 WHERE a.student = :student) ORDER BY u.lastName, u.firstName')
+            ->setParameter('student', $student)
+            ->getResult();
+    }
+    public function getEducationalTutorsByDepartmentsAgreementSummary(Collection $departments)
+    {
+        $em = $this->getEntityManager();
+        return $em->createQuery('SELECT u,
+              (SELECT COUNT(a1) FROM AppBundle:Agreement a1 WHERE a1.educationalTutor = u),
+              (SELECT COUNT(DISTINCT a2.student) FROM AppBundle:Agreement a2 WHERE a2.educationalTutor = u)
+              FROM AppBundle:User u WHERE u IN (SELECT DISTINCT IDENTITY(a.educationalTutor) FROM AppBundle:Agreement a JOIN AppBundle:User s WITH a.student = s JOIN AppBundle:Group g WITH s.studentGroup = g JOIN AppBundle:Training t WITH g.training = t JOIN AppBundle:Department d WITH t.department = d WHERE d IN (:departments)) ORDER BY u.lastName, u.firstName')
+            ->setParameter('departments', $departments)
+            ->getResult();
+    }
+    public function getEducationalTutorsExpenseSummary()
+    {
+        $em = $this->getEntityManager();
+        return $em->createQuery('SELECT u,
+              (SELECT SUM(e1.distance) FROM AppBundle:Expense e1 WHERE e1.teacher = u),
+              (SELECT SUM(e2.distance * e2.reviewed) FROM AppBundle:Expense e2 WHERE e2.teacher = u),
+              (SELECT SUM(e3.distance * e3.paid) FROM AppBundle:Expense e3 WHERE e3.teacher = u)
+              FROM AppBundle:User u WHERE u IN (SELECT DISTINCT IDENTITY(a.educationalTutor) FROM AppBundle:Agreement a) ORDER BY u.lastName, u.firstName')
+            ->getResult();
+    }
+    public function getEducationalTutorsByDepartmentsExpenseSummary(Collection $departments)
+    {
+        $em = $this->getEntityManager();
+        return $em->createQuery('SELECT u,
+              (SELECT SUM(e1.distance) FROM AppBundle:Expense e1 WHERE e1.teacher = u),
+              (SELECT SUM(e2.distance * e2.reviewed) FROM AppBundle:Expense e2 WHERE e2.teacher = u),
+              (SELECT SUM(e3.distance * e3.paid) FROM AppBundle:Expense e3 WHERE e3.teacher = u)
+              FROM AppBundle:User u WHERE u IN (SELECT DISTINCT IDENTITY(a.educationalTutor) FROM AppBundle:Agreement a JOIN AppBundle:User s WITH a.student = s JOIN AppBundle:Group g WITH s.studentGroup = g JOIN AppBundle:Training t WITH g.training = t JOIN AppBundle:Department d WITH t.department = d WHERE d IN (:departments)) ORDER BY u.lastName, u.firstName')
+            ->setParameter('departments', $departments)
+            ->getResult();
+    }
+    public function getStudentsByWorkcenterAndEducationalTutor(Workcenter $workcenter = null, User $tutor = null)
+    {
+        if (null === $workcenter || null === $tutor) {
+            return [];
+        }
+        $em = $this->getEntityManager();
+        return $em->createQuery('SELECT u
+              FROM AppBundle:User u WHERE u IN (SELECT DISTINCT IDENTITY(a.student) FROM AppBundle:Agreement a WHERE a.workcenter = :workcenter AND a.educationalTutor = :tutor) ORDER BY u.lastName, u.firstName')
+            ->setParameter('workcenter', $workcenter)
+            ->setParameter('tutor', $tutor)
+            ->getResult();
+    }
+}
---
+++ src/AppBundle/Entity/Usuario.php
@@ -0,0 +1,253 @@
+<?php
+// src/AppBundle/Entity/User.php
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="Usuario")
+ */
+class Usuario extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=false)
+     *
+     * @var boolean
+     */
+    protected $admin = false;
+    /**
+     * @ORM\Column(type="boolean", nullable=false)
+     *
+     * @var boolean
+     */
+    protected $validator = false;
+    /**
+     * @ORM\Column(type="string", length=255, nullable=false)
+     *
+     * @Assert\NotBlank(message="Por favor, introduce tu nombre.", groups={"Registration", "Profile"})
+     * @Assert\Length(
+     *     min=3,
+     *     max=255,
+     *     minMessage="El nombre es demasiado corto. Introduce como mínimo 3 caracteres.",
+     *     maxMessage="El nombre es demasiado largo, no debe sobrepasar los 255 caracteres.",
+     *     groups={"Registration", "Profile"}
+     * )
+     */
+    protected $nombre;
+    /**
+     * @ORM\Column(type="string", length=255, nullable=false)
+     *
+     * @Assert\NotBlank(message="Por favor, introduce al menos un apellido.", groups={"Registration", "Profile"})
+     * @Assert\Length(
+     *     min=3,
+     *     max=255,
+     *     minMessage="El/los apellido/s son demasiado cortos",
+     *     maxMessage="El/los apellido/s son demasiado largos",
+     *     groups={"Registration", "Profile"}
+     * )
+     */
+    protected $apellidos;
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     *
+     * @var /date
+     */
+    protected $fechaNacimiento;
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     *
+     * @var string
+     */
+    protected $imagenPerfil;
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     *
+     * @var \DateTime
+     */
+    protected $activacion;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function getUsername()
+    {
+        return $this->email;
+    }
+    public function getSalt()
+    {
+        return null;
+    }
+    public function getRoles()
+    {
+        $roles = ["ROLE_USER"];     //Todos los usuarios son ROLE_USER
+        if($this->isAdmin()){
+            $roles[] = "ROLE_ADMIN";
+        }
+        if($this->isValidator()){
+            $roles[] = "ROLE_VALIDATOR";
+        }
+        return $roles;
+    }
+    public function eraseCredentials()
+    {
+    }
+
+    public function serialize()
+    {
+        return serialize(array(
+            $this->id,
+            $this->email,
+            $this->password
+        ));
+    }
+    public function  unserialize($serialized)
+    {
+        list(
+            $this->id,
+            $this->email,
+            $this->password
+            ) = unserialize($serialized);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isAdmin()
+    {
+        return $this->admin;
+    }
+
+    /**
+     * @param bool $admin
+     */
+    public function setAdmin($admin)
+    {
+        $this->admin = $admin;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isValidator()
+    {
+        return $this->validator;
+    }
+
+    /**
+     * @param bool $validator
+     */
+    public function setValidator($validator)
+    {
+        $this->validator = $validator;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * @param mixed $nombre
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getApellidos()
+    {
+        return $this->apellidos;
+    }
+
+    /**
+     * @param mixed $apellidos
+     */
+    public function setApellidos($apellidos)
+    {
+        $this->apellidos = $apellidos;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFechaNacimiento()
+    {
+        return $this->fechaNacimiento;
+    }
+
+    /**
+     * @param mixed $fechaNacimiento
+     */
+    public function setFechaNacimiento($fechaNacimiento)
+    {
+        $this->fechaNacimiento = $fechaNacimiento;
+    }
+
+    /**
+     * @return string
+     */
+    public function getImagenPerfil()
+    {
+        return $this->imagenPerfil;
+    }
+
+    /**
+     * @param string $imagenPerfil
+     */
+    public function setImagenPerfil($imagenPerfil)
+    {
+        $this->imagenPerfil = $imagenPerfil;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getActivacion()
+    {
+        return $this->activacion;
+    }
+
+    /**
+     * @param \DateTime $activacion
+     */
+    public function setActivacion($activacion)
+    {
+        $this->activacion = $activacion;
+    }
+
+
+}
